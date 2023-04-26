39f8bdea99283c3cb8e7e7bac01a9cdad361bc1c
rpomponio <raymond.pomponio@cuanschutz.edu>
2023-04-26 15:01:56 -0600

    week 6 progress

--- MyNeuralNetwork.py
+++ MyNeuralNetwork.py
@@ -5,7 +5,7 @@ class NeuralNetwork(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.main = torch.nn.Sequential(
-            torch.nn.Linear(in_features = 28 * 28, out_features=512),
+            torch.nn.Linear(in_features=28 * 28, out_features=512),
             torch.nn.ReLU(),
             torch.nn.Linear(512, 512),
             torch.nn.ReLU(),
--- README.md
+++ README.md
@@ -20,3 +20,10 @@ Code examples from working group in Neural Network Workshop Series
 # Week 4 Summary (Mar 29)
 - Downloaded Fashion MNIST dataset
 - Setup code for training a network in 3 epochs
+
+# Week 5 (Apr 12)
+- Missed this week
+
+# Week 6 Summary (Apr 26)
+- Began training network for various learning rates
+- Used Tensorboard to evaluate training performance
